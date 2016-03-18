a4a98c1f3e2d7cb6012094d30aca3b2bbb96707a
Marco Lauinger <marco.lauinger@gmail.com>
2016-03-18 23:36:45 +0100

    made settings popup dynamic

--- app/src/main/java/de/mlauinger/studienarbeit/javadrone/activity/ControlSettings.java
+++ app/src/main/java/de/mlauinger/studienarbeit/javadrone/activity/ControlSettings.java
@@ -1,25 +1,43 @@
 package de.mlauinger.studienarbeit.javadrone.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import de.mlauinger.studienarbeit.javadrone.R;
 
 public class ControlSettings extends AppCompatActivity {
 
+    private FrameLayout settingsContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_control_settings);
+        getFragmentManager().beginTransaction()
+                .add(R.id.settings_fragment, new SettingsImageFragment())
+                .commit();
+        settingsContainer = (FrameLayout) findViewById(R.id.settings_fragment);
     }
 
     public void switchScreen(View view) {
     }
 
     public void showSettings(View view) {
-        getFragmentManager().beginTransaction()
-                .add(R.id.settings_fragment, new SettingsImageFragment())
-                .commit();
+        if(settingsContainer.isShown()) {
+            settingsContainer.setVisibility(View.INVISIBLE);
+            return;
+        }
+        settingsContainer.setVisibility(View.VISIBLE);
+
+    }
+
+    public void doEmergency(View view) {
+    }
+
+    public void showDroneConfiguration(View view) {
     }
 }
