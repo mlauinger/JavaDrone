8b4f68c4e2291847a2008d3d7cccb69915efd9fd
Marco Lauinger <marco.lauinger@gmail.com>
2016-03-18 13:33:35 +0100

    added base layout for second control screen

---
+++ app/src/main/java/de/mlauinger/studienarbeit/javadrone/activity/ControlSettings.java
@@ -0,0 +1,25 @@
+package de.mlauinger.studienarbeit.javadrone.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import de.mlauinger.studienarbeit.javadrone.R;
+
+public class ControlSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_settings);
+    }
+
+    public void switchScreen(View view) {
+    }
+
+    public void showSettings(View view) {
+    }
+}
