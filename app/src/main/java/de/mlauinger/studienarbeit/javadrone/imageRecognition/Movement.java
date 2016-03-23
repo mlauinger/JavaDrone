52fb87adc991d7af7d2dfb583ee532ad8b8fe277
Marco Lauinger <marco.lauinger@gmail.com>
2016-03-23 12:51:39 +0100

    adapted values

--- app/src/main/java/de/mlauinger/studienarbeit/javadrone/imageRecognition/Movement.java
+++ app/src/main/java/de/mlauinger/studienarbeit/javadrone/imageRecognition/Movement.java
@@ -21,7 +21,7 @@ Values:
     public void followCircle(CirclePos circle, DroneController controller) {
         //Circle not detected
         counter++;
-        if (counter < 10) {
+        if (counter < 2) {
             return;
         }
         counter = 0;
@@ -33,16 +33,16 @@ Values:
         if (calculateDeltaHor(circle) > circle.getWidth() / PERSENTAGE) {
 
             //Turn left to move Circle to the Center of the Image
-            if (circle.getPosx() - circle.getWidth() / 2 < 0) {
+            if (circle.getPosx() - circle.getWidth() / 2 > 0) {
                 System.out.println("Turn left");
-                controller.turnDrone(-0.01f);
+                controller.turnDrone(-0.005f);
                 return;
             }
 
             //Turn right to move Circle to the Center of the Image
-            if (circle.getPosx() - circle.getWidth() / 2 > 0) {
+            if (circle.getPosx() - circle.getWidth() / 2 < 0) {
                 System.out.println("Turn right");
-                controller.turnDrone(0.01f);
+                controller.turnDrone(0.005f);
                 return;
             }
 
@@ -54,7 +54,7 @@ Values:
             //get closer to Circle to adjust the size of the circle
             if (circle.getRadius() < circle.getWidth() / CIRCLE_SIZE) {
                 System.out.println("move closer");
-                //controller.flyDrone(-0.01f, 0);
+                controller.flyDrone(-0.005f, 0);
                 return;
             }
 
@@ -62,7 +62,7 @@ Values:
             //move away to Circle to adjust the size of the circle
             if (circle.getRadius() > circle.getWidth() / CIRCLE_SIZE) {
                 System.out.println("move away");
-                controller.flyDrone(0.01f, 0);
+                controller.flyDrone(0.005f, 0);
                 return;
             }
         }
